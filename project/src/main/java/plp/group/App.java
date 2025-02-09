3b903f2cc970e21e9bcdcee4cc09355255e04494
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-02-08 19:45:16 -0500

    Read Program From Resource File

--- project/src/main/java/plp/group/App.java
+++ project/src/main/java/plp/group/App.java
@@ -1,7 +1,5 @@
 package plp.group;
 
-import javax.swing.JFrame;
-
 import org.antlr.v4.gui.Trees;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -16,24 +14,16 @@ import plp.group.project.pascalParser;
  */
 public class App {
     public static void main(String[] args) {
-        // Use a simple hello world program to test
-        var program = """
-                program Hello;
-                begin
-                  writeln ('Hello, world.');
-                end.
-                """;
-
         try {
-            // Get the parse tree.
-            var lexer = new pascalLexer(CharStreams.fromString(program)); // CharStreams.fromFileName(args[0])
+            // Get the parse tree for the file we enter in command line.
+            var lexer = new pascalLexer(
+                    CharStreams.fromStream(App.class.getClassLoader().getResourceAsStream(args[0])));
             var tokens = new CommonTokenStream(lexer);
             var parser = new pascalParser(tokens);
             var tree = parser.program();
 
             // Open a GUI window with the parse tree.
-            JFrame frame = Trees.inspect(tree, parser).get();
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            Trees.inspect(tree, parser);
         } catch (Exception e) {
             System.out.println(e.toString());
         }
