9f0c9db224566fa523e32353d153fd8d27e46dd4
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-02-11 17:21:31 -0500

    Control flow for expressions - needs operator logic

--- project/src/main/java/plp/group/Interpreter/Interpreter.java
+++ project/src/main/java/plp/group/Interpreter/Interpreter.java
@@ -8,7 +8,11 @@ import java.util.Scanner;
 import com.ibm.icu.impl.Pair;
 
 import plp.group.project.delphiBaseVisitor;
+import plp.group.project.delphiLexer;
 import plp.group.project.delphiParser;
+import plp.group.project.delphiParser.AdditiveoperatorContext;
+import plp.group.project.delphiParser.MultiplicativeoperatorContext;
+import plp.group.project.delphiParser.RelationaloperatorContext;
 
 /**
  * Interpret valid delphi code according to our grammar.
@@ -92,12 +96,6 @@ public class Interpreter extends delphiBaseVisitor<Object> {
 
     // #region Declarations
 
-    @Override
-    public Object visitLabelDeclarationPart(delphiParser.LabelDeclarationPartContext ctx) {
-        // TODO: figure out how to store labels and what they are used for.
-        return visitChildren(ctx);
-    }
-
     /**
      * Inserts each of the identifiers within the variable declaration to the
      * current scope with no value but specified type.
@@ -109,9 +107,9 @@ public class Interpreter extends delphiBaseVisitor<Object> {
         var type = getJavaType(ctx.getChild(ctx.getChildCount() - 1).getText());
 
         // For each identifier, insert it to the current scope with the type set.
-        var identifiers = (ArrayList<SymbolInfo>) visit(ctx.getChild(0));
-        for (SymbolInfo ident : identifiers) {
-            scope.insert(ident.name, new SymbolInfo(ident.name, null, type));
+        var identifiers = (ArrayList<String>) visit(ctx.getChild(0));
+        for (String ident : identifiers) {
+            scope.insert(ident, new SymbolInfo(ident, null, type));
         }
 
         return null;
@@ -123,123 +121,244 @@ public class Interpreter extends delphiBaseVisitor<Object> {
      * Returns null.
      */
     @Override
-    public Object visitProcedureDeclaration(delphiParser.ProcedureDeclarationContext ctx) {
-        var procedureName = (SymbolInfo) visit(ctx.getChild(1));
-
-        var paramsList = visit(ctx.getChild(2)); // TODO: implement visitFormalParameterList
-
-        var body = ctx.getChild(4);
-
-        SymbolTable capturedScope = scope;
-        scope.insert(procedureName.name, new SymbolInfo(
-                procedureName.name,
-                (List<Object> arguments) -> {
-                    // Create the new function scope (store current scope to restore later)
-                    SymbolTable currentScope = scope;
-                    scope = capturedScope;
-                    scope.enterScope();
-
-                    try {
-                        // Add the arguments
-                        for (int i = 0; i < arguments.size(); i++) {
-                            scope.insert(null, new SymbolInfo(null, arguments.get(i), null)); // TODO: add type info
-                        }
-
-                        // Visit the body
-                        visit(body);
-                    } finally { // TODO: Error handling here??
-                        // Restore scope no matter what
-                        scope.exitScope();
-                        scope = currentScope;
-                    }
-
-                    return null;
-                },
-                List.of(Object[].class), // TODO: update the list of arguments here to be correct...
-                Void.class));
-
-        return null;
+    public Object visitProcedureDeclaration(delphiParser.ProcedureDeclarationContext ctx) { // TODO: PROPERLY IMPLEMENT
+        return visitChildren(ctx);
+        /*
+         * var procedureName = (SymbolInfo) visit(ctx.getChild(1));
+         * 
+         * var paramsList = visit(ctx.getChild(2)); // TODO: implement
+         * visitFormalParameterList
+         * 
+         * var body = ctx.getChild(4);
+         * 
+         * SymbolTable capturedScope = scope;
+         * scope.insert(procedureName.name, new SymbolInfo(
+         * procedureName.name,
+         * (List<Object> arguments) -> {
+         * // Create the new function scope (store current scope to restore later)
+         * SymbolTable currentScope = scope;
+         * scope = capturedScope;
+         * scope.enterScope();
+         * 
+         * try {
+         * // Add the arguments
+         * for (int i = 0; i < arguments.size(); i++) {
+         * scope.insert(null, new SymbolInfo(null, arguments.get(i), null)); // TODO:
+         * add type info
+         * }
+         * 
+         * // Visit the body
+         * visit(body);
+         * } finally { // TODO: Error handling here??
+         * // Restore scope no matter what
+         * scope.exitScope();
+         * scope = currentScope;
+         * }
+         * 
+         * return null;
+         * },
+         * List.of(Object[].class), // TODO: update the list of arguments here to be
+         * correct...
+         * Void.class));
+         * 
+         * return null;
+         */
     }
 
     @Override
-    public Object visitFunctionDeclaration(delphiParser.FunctionDeclarationContext ctx) {
+    public Object visitFunctionDeclaration(delphiParser.FunctionDeclarationContext ctx) { // TODO: PROPERLY IMPLEMENT
         return visitChildren(ctx);
     }
 
     // #endregion Declarations
 
-    @Override
-    public Object visitFormalParameterList(delphiParser.FormalParameterListContext ctx) {
-        return visitChildren(ctx);
-    }
-
-    @Override
-    public Object visitFormalParameterSection(delphiParser.FormalParameterSectionContext ctx) {
-        return visitChildren(ctx);
-    }
+    // #region Identifiers
 
     /**
-     * Returns a list of SymbolInfo. The last element is the type (as it appears in
-     * the
-     * Pascal program).
+     * Returns the SymbolInfo for the variable.
      */
     @Override
-    public Object visitParameterGroup(delphiParser.ParameterGroupContext ctx) {
-        var identifiers = (ArrayList<SymbolInfo>) visit(ctx.getChild(0));
-        var type = (SymbolInfo) visit(ctx.getChild(2));
-        return identifiers.add(type);
+    public Object visitVariable(delphiParser.VariableContext ctx) {
+        var name = (String) visit(ctx.getChild(0));
+        return scope.lookup(name);
     }
-
-    // #region Identifiers
 
     /**
      * Returns the result of visiting all the identifiers in a list.
      */
     @Override
     public Object visitIdentifierList(delphiParser.IdentifierListContext ctx) {
-        List<SymbolInfo> idents = new ArrayList<SymbolInfo>();
+        List<String> identifiers = new ArrayList<String>();
         for (var i = 0; i < ctx.getChildCount(); i += 2) { // Iterate by 2s to skip each COMMA
-            var ident = (SymbolInfo) visit(ctx.getChild(i));
-            idents.add(ident);
+            identifiers.add((String) visit(ctx.getChild(i)));
         }
-        return idents;
+        return identifiers;
     }
 
     /**
-     * Returns a SymbolInfo struct.
-     * 
-     * If in the scope, it will be filled out.
-     * If not, it will have null fields but the name is there.
+     * Returns a string of the identifier name.
      */
     @Override
     public Object visitIdentifier(delphiParser.IdentifierContext ctx) {
-        var info = scope.lookup(ctx.getText());
-        if (info == null) {
-            info = new SymbolInfo(ctx.getText(), null);
-        }
-        return info;
+        return ctx.getText();
     }
 
     // #endregion Identifiers
 
     // Related to procedures
 
+    // #region Statements
+
     @Override
     public Object visitProcedureStatement(delphiParser.ProcedureStatementContext ctx) {
         // Get the procedure name/details
-        SymbolInfo procedureDetails = (SymbolInfo) scope.lookup(((SymbolInfo) visit(ctx.getChild(0))).name);
-
-        System.out.println(((SymbolInfo) visit(ctx.getChild(0))).name);
+        var procedureDetails = (SymbolInfo) scope.lookup(((String) visit(ctx.getChild(0))));
 
         // Visit parameters to get values to add to the parameters list
         var parameters = new ArrayList<Object>();
         for (var i = 2; i < ctx.getChildCount() - 1; i++) {
-            parameters.add(visit(ctx.getChild(i)));
+            var parameter = visit(ctx.getChild(i));
+            if (parameter instanceof SymbolInfo) {
+                parameter = ((SymbolInfo) parameter).value; // If parameter is a SymbolInfo, grab its value
+            }
+            parameters.add(parameter);
         }
 
         // Call the function and return the result.
         return procedureDetails.function.apply(parameters);
     }
+
+    @Override
+    public Object visitAssignmentStatement(delphiParser.AssignmentStatementContext ctx) {
+        var identifier = (SymbolInfo) visit(ctx.getChild(0));
+        var value = visit(ctx.getChild(2));
+
+        scope.update(identifier.name, new SymbolInfo(identifier.name, value));
+        return null;
+    }
+
+    // #endregion Statements
+
+    // #region Expressions
+
+    /**
+     * Visits the expression, returns whatever the value of the expression is.
+     */
+    @Override
+    public Object visitExpression(delphiParser.ExpressionContext ctx) {
+        var lhs = visit(ctx.getChild(0));
+
+        if (ctx.relationaloperator() == null) {
+            return lhs;
+        }
+
+        var operator = (String) visit(ctx.relationaloperator());
+        var rhs = visit(ctx.getChild(2));
+
+        return switch (operator) { // TODO: add logic
+            case "==" -> 1;
+            case "!=" -> 2;
+            case "<" -> 3;
+            case "<=" -> 4;
+            case ">" -> 5;
+            case ">=" -> 6;
+            case "IN" -> 7;
+            default -> throw new RuntimeException("Unhandled or unknown operator: " + operator);
+        };
+    }
+
+    /**
+     * Get the string representation (text) of the relational operator.
+     */
+    @Override
+    public Object visitRelationaloperator(RelationaloperatorContext ctx) {
+        return ctx.getText();
+    }
+
+    /**
+     * Visits the simpleExpression, returns whatever the value of the
+     * simpleExpression is.
+     */
+    @Override
+    public Object visitSimpleExpression(delphiParser.SimpleExpressionContext ctx) {
+        var lhs = visit(ctx.getChild(0));
+
+        if (ctx.additiveoperator() == null) {
+            return lhs;
+        }
+
+        var operator = (String) visit(ctx.additiveoperator());
+        var rhs = visit(ctx.getChild(2));
+
+        return switch (operator) { // TODO: add logic
+            case "+" -> 1;
+            case "-" -> 2;
+            case "OR" -> 3;
+            default -> throw new RuntimeException("Unhandled or unknown operator: " + operator);
+        };
+    }
+
+    /**
+     * Get the string representation (text) of the additive operator.
+     */
+    @Override
+    public Object visitAdditiveoperator(AdditiveoperatorContext ctx) {
+        return ctx.getText();
+    }
+
+    @Override
+    public Object visitTerm(delphiParser.TermContext ctx) {
+        var lhs = visit(ctx.getChild(0));
+
+        if (ctx.multiplicativeoperator() == null) {
+            return lhs;
+        }
+
+        var operator = (String) visit(ctx.multiplicativeoperator());
+        var rhs = visit(ctx.getChild(2));
+
+        return switch (operator) { // TODO: add logic
+            case "*" -> 1;
+            case "/" -> 2;
+            case "MOD" -> 3;
+            case "DIV" -> 4;
+            case "AND" -> 5;
+            default -> throw new RuntimeException("Unhandled or unknown operator: " + operator);
+        };
+    }
+
+    /**
+     * Get the string representation (text) of the multiplicative operator.
+     */
+    @Override
+    public Object visitMultiplicativeoperator(MultiplicativeoperatorContext ctx) {
+        return ctx.getText();
+    }
+
+    @Override
+    public Object visitSignedFactor(delphiParser.SignedFactorContext ctx) {
+        // TODO: figure out how to grab sign and apply to factor
+        var factor = visit(ctx.factor());
+        return factor;
+    }
+
+    @Override
+    public Object visitFactor(delphiParser.FactorContext ctx) {
+        return visitChildren(ctx); // TODO: actually do all the below...
+        /*
+         * factor
+         * : variable
+         * | LPAREN expression RPAREN
+         * | functionDesignator
+         * | unsignedConstant
+         * | set_
+         * | NOT factor
+         * | bool_
+         * ;
+         */
+    }
+
+    // #endregion Expressions
 
     // Related to constants
 
@@ -247,6 +366,21 @@ public class Interpreter extends delphiBaseVisitor<Object> {
     public Object visitString(delphiParser.StringContext ctx) {
         var literal = ctx.STRING_LITERAL().getText();
         return literal.substring(1, literal.length() - 1);
+    }
+
+    @Override
+    public Object visitUnsignedInteger(delphiParser.UnsignedIntegerContext ctx) {
+        return Integer.parseInt(ctx.NUM_INT().toString());
+    }
+
+    @Override
+    public Object visitUnsignedReal(delphiParser.UnsignedRealContext ctx) {
+        return new BigDecimal(ctx.NUM_REAL().toString());
+    }
+
+    @Override
+    public Object visitConstantChr(delphiParser.ConstantChrContext ctx) {
+        return ctx.CHR().getText().charAt(0);
     }
 
     // #region Helper Functions
