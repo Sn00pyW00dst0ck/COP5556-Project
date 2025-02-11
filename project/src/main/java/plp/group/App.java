f197fda61e96ba868e98285d9ca48c50713ec75b
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-02-11 12:47:10 -0500

    Update main to be easier to use to test many things at once

--- project/src/main/java/plp/group/App.java
+++ project/src/main/java/plp/group/App.java
@@ -1,5 +1,7 @@
 package plp.group;
 
+import java.util.Scanner;
+
 import org.antlr.v4.gui.Trees;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -15,22 +17,37 @@ import plp.group.project.delphiParser;
  */
 public class App {
     public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("Enter a program to interpret.");
+                System.out.print("> ");
+                var input = scanner.nextLine();
+                if (input == "exit") {
+                    break;
+                }
+                System.out.println("Lexing, parsing, and interpreting.");
+                interpretProgram(input);
+                System.out.println("Finished.");
+                System.out.println("");
+            }
+        }
+    }
+
+    private static void interpretProgram(String programFileName) {
         try {
             // Get the parse tree for the file we enter in command line.
             var lexer = new delphiLexer(
-                    CharStreams.fromStream(App.class.getClassLoader().getResourceAsStream(args[0])));
+                    CharStreams.fromStream(App.class.getClassLoader().getResourceAsStream(programFileName)));
             var tokens = new CommonTokenStream(lexer);
             var parser = new delphiParser(tokens);
             var tree = parser.program();
 
             // Open a GUI window with the parse tree & print it to command line.
             // System.out.println(tree.toStringTree(parser));
-            Trees.inspect(tree, parser);
+            // Trees.inspect(tree, parser);
 
-            System.out.println("START INTERPRETING!");
             var interpreter = new Interpreter();
             interpreter.visit(tree);
-            System.out.println("DONE INTERPRETING!");
         } catch (Exception e) {
             System.out.println(e.toString());
         }
