73e41c69890647e228baaf52082f7304e280f77c
Rishika Sharma <rishikasharma0064@gmail.com>
2025-02-11 20:04:36 -0500

    added class declaration

--- project/src/main/java/plp/group/Interpreter/Interpreter.java
+++ project/src/main/java/plp/group/Interpreter/Interpreter.java
@@ -66,6 +66,63 @@ public class Interpreter extends delphiBaseVisitor<Object> {
     }
 
     @Override
+    public Object visitClassDeclaration(delphiParser.ClassDeclarationContext ctx) {
+        String className = ctx.IDENTIFIER().getText();
+        SymbolTable classScope = new SymbolTable(); // Store attributes and methods inside class scope
+        
+        for (var member : ctx.classBody().children) {
+            if (member instanceof delphiParser.VariableDeclarationContext) {
+                visitVariableDeclaration((delphiParser.VariableDeclarationContext) member);
+            } else if (member instanceof delphiParser.MethodDeclarationContext) {
+                visitMethodDeclaration((delphiParser.MethodDeclarationContext) member);
+            } else if (member instanceof delphiParser.ConstructorDeclarationContext) {
+                visitConstructorDeclaration((delphiParser.ConstructorDeclarationContext) member);
+            }
+        }
+        
+        scope.insert(className, new SymbolInfo(className, classScope, null)); // Store class info
+        return null;
+    }
+
+    @Override
+    public Object visitObjectInstantiation(delphiParser.ObjectInstantiationContext ctx) {
+        String objectName = ctx.IDENTIFIER(0).getText();
+        String className = ctx.IDENTIFIER(1).getText();
+        
+        SymbolInfo classInfo = scope.lookup(className);
+        if (classInfo == null || !(classInfo.value instanceof SymbolTable)) {
+            throw new RuntimeException("Class " + className + " is not defined.");
+        }
+        
+        SymbolTable objectScope = new SymbolTable();
+        objectScope.copyFrom((SymbolTable) classInfo.value); // Copy class members
+        
+        // If constructor exists, call it
+        SymbolInfo constructor = objectScope.lookup(className);
+        if (constructor != null && constructor.value instanceof Function) {
+            ((Function) constructor.value).apply(new ArrayList<>()); // Pass arguments if any
+        }
+        
+        scope.insert(objectName, new SymbolInfo(objectName, objectScope, null)); // Store object
+        return null;
+    }
+
+    @Override
+    public Object visitConstructorDeclaration(delphiParser.ConstructorDeclarationContext ctx) {
+        String className = ctx.getParent().getChild(0).getText(); // Parent is class
+        SymbolTable classScope = (SymbolTable) scope.lookup(className).value;
+        
+        classScope.insert(className, new SymbolInfo(className, (Function<List<Object>>) args -> {
+            scope.enterScope(); // Enter object scope
+            visit(ctx.block()); // Run constructor code
+            scope.exitScope();
+            return null;
+        }, List.of(), Void.class));
+        
+        return null;
+    }
+
+    @Override
     public Object visitIdentifier(delphiParser.IdentifierContext ctx) {
         return scope.lookup(ctx.getText());
     }
