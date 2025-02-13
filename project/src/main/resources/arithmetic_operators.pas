c805bfc830170db97548ec2f48e4e99c5ccc16c1
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-02-13 17:17:50 -0500

    Add pas files for new tests

---
+++ project/src/main/resources/arithmetic_operators.pas
@@ -0,0 +1,17 @@
+program Arithmetic_Operators;
+begin
+  writeln (3 + 4);
+  writeln (6 + 9.5);
+  writeln ('Hello World' + '!');
+  writeln ('Hello World' + CHR(60));
+  writeln (CHR(65) + CHR(66));
+  
+  writeln (3 - 4);
+  writeln (3 - 4.5);
+  
+  writeln (3 * 4);
+  writeln (3 * 4.5);
+  
+  writeln (4 / 2);
+  writeln (6.0 / 1.5);
+end.
