bcf208e6f05ef60e791a8276ec139a78030522bf
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-03-29 21:10:12 -0400

    Update main application commands for optional optimizations

--- project/src/main/java/plp/group/App.java
+++ project/src/main/java/plp/group/App.java
@@ -1,5 +1,6 @@
 package plp.group;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import org.antlr.v4.gui.Trees;
@@ -23,16 +24,19 @@ public class App {
             while (true) {
                 System.out.println("Enter your command to perform.");
                 System.out.print("> ");
-                var input = scanner.nextLine().split(" ");
 
-                switch (input[0]) {
+                String[] input = scanner.nextLine().split(" ");
+                String command = input[0];
+                String argument = (input.length > 2 && "-o".equals(input[1])) ? input[2] : (input.length > 1 ? input[1] : null);
+
+                switch (command) {
                     case "exit":
                         return;
                     case "eval":
-                        interpretProgram(input[1]);
+                        interpretProgram(argument, "-o".equals(input[1]));
                         break;
                     case "tree":
-                        displayParseTree(input[1]);
+                        displayParseTree(argument, "-o".equals(input[1]));
                         break;
                     case "help":
                         displayHelpMenu();
@@ -45,7 +49,7 @@ public class App {
         }
     }
 
-    private static void interpretProgram(String programFileName) {
+    private static void interpretProgram(String programFileName, boolean optimize) {
         try {
             // Get the parse tree for the file we enter in command line.
             var lexer = new delphi_lexer(
@@ -53,6 +57,13 @@ public class App {
             var tokens = new CommonTokenStream(lexer);
             var parser = new delphi(tokens);
             var tree = parser.program();
+
+            // Apply optimization pass if necessary.
+            if (optimize) {
+                String optimized = (new Optimizer()).visit(tree);
+                tree = new delphi(new CommonTokenStream(new delphi_lexer(CharStreams.fromString(optimized)))).program();
+            }
+
             var interpreter = new Interpreter();
             interpreter.visit(tree);
         } catch (Exception e) {
@@ -60,7 +71,7 @@ public class App {
         }
     }
 
-    private static void displayParseTree(String programFileName) {
+    private static void displayParseTree(String programFileName, boolean optimize) {
         try {
             // Get the parse tree for the file we enter in command line.
             var lexer = new delphi_lexer(
@@ -69,18 +80,23 @@ public class App {
             var parser = new delphi(tokens);
             var tree = parser.program();
 
-            String optimized = (new Optimizer()).visit(tree);
-            System.out.println(optimized);
-            var tree2 = new delphi(new CommonTokenStream(new delphi_lexer(CharStreams.fromString(optimized)))).program();
+            // Apply optimization pass if necessary.
+            if (optimize) {
+                String optimized = (new Optimizer()).visit(tree);
+                tree = new delphi(new CommonTokenStream(new delphi_lexer(CharStreams.fromString(optimized)))).program();
+            }
 
             // Open a GUI window with the parse tree.
-            var frame = Trees.inspect(tree2, parser);
+            var frame = Trees.inspect(tree, parser);
             frame.get().setSize(600, 800);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Show the application's help menu.
+     */
     private static void displayHelpMenu() {
         System.out.println("Help Menu:");
         System.out.println("--------------------");
@@ -88,10 +104,14 @@ public class App {
         System.out.println("\tQuits the program.\n");
         System.out.println("eval <program_file>");
         System.out.println("\tInterpret the program_file.\n");
+        System.out.println("eval -o <program_file>");
+        System.out.println("\tInterpret the program_file after optimizations are applied.\n");
         System.out.println("help");
         System.out.println("\tDisplays this help menu.\n");
         System.out.println("tree <program_file>");
         System.out.println("\tDisplays the parsed tree for the program_file in a GUI window.");
+        System.out.println("tree -o <program_file>");
+        System.out.println("\tDisplays the parsed tree for the program_file after optimizations are applied in a GUI window.");
         System.out.println("--------------------");
     }
 
