43686b978359673b4b21ab1fb8b57bbab697ad0c
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-04-04 21:02:26 -0400

    Add new built in math functions based on pascal resources

--- project/src/main/java/plp/group/Interpreter/Environment.java
+++ project/src/main/java/plp/group/Interpreter/Environment.java
@@ -1,12 +1,16 @@
 package plp.group.Interpreter;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.jline.reader.LineReader;
 
+import ch.obermuhlner.math.big.BigDecimalMath;
 import plp.group.Interpreter.ControlFlowExceptions.BreakException;
 import plp.group.Interpreter.ControlFlowExceptions.ContinueException;
 import plp.group.Interpreter.ControlFlowExceptions.ReturnException;
@@ -226,13 +230,240 @@ public class Environment {
             (Scope _) -> { throw new ContinueException(); }
         ));
 
+        // Following added based on this: https://www.dcs.ed.ac.uk/home/SUNWspro/3.0/pascal/lang_ref/ref_builtin.doc.html
+        // The only ones from that table not added are ones with multiple signatures.
+        
+        // https://www.freepascal.org/docs-html/rtl/system/arctan.html
+        scope.define("arctan/1", new RuntimeValue.Method(
+            "arctan/1",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(
+                    new RuntimeValue.Method.MethodParameter(
+                        "x",
+                        new RuntimeValue.Primitive(new BigDecimal("0.0")),
+                        false,
+                        false
+                    )
+                ), 
+                new RuntimeValue.Primitive(new BigDecimal("0.0"))
+            ),
+            (Scope methodScope) -> {
+                BigDecimal num = RuntimeValue.requireType(RuntimeValue.requireType(methodScope.lookup("x").get(), RuntimeValue.Variable.class), BigDecimal.class);
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive(BigDecimalMath.atan(num, new MathContext(20)));
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
+        
+        scope.define("chr/1", new RuntimeValue.Method(
+            "chr/1",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(
+                    new RuntimeValue.Method.MethodParameter(
+                        "c",
+                        new RuntimeValue.Primitive(new BigInteger("0")),
+                        false,
+                        false
+                    )
+                ),
+                new RuntimeValue.Primitive(Character.valueOf('a'))
+            ),
+            (Scope methodScope) -> {
+                BigInteger num = RuntimeValue.requireType(RuntimeValue.requireType(methodScope.lookup("c").get(), RuntimeValue.Variable.class), BigInteger.class);
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive((char) num.intValue());
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
+        
+        // https://www.freepascal.org/docs-html/rtl/system/cos.html
+        scope.define("cos/1", new RuntimeValue.Method(
+            "cos/1",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(
+                    new RuntimeValue.Method.MethodParameter(
+                        "theta",
+                        new RuntimeValue.Primitive(new BigDecimal("0.0")),
+                        false,
+                        false
+                    )
+                ),
+                new RuntimeValue.Primitive(new BigDecimal("0.0"))
+            ),
+            (Scope methodScope) -> {
+                BigDecimal num = RuntimeValue.requireType(RuntimeValue.requireType(methodScope.lookup("theta").get(), RuntimeValue.Variable.class), BigDecimal.class);
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive(BigDecimalMath.cos(num, new MathContext(20)));
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
+        
+        // https://www.freepascal.org/docs-html/rtl/system/exp.html
+        scope.define("exp/1", new RuntimeValue.Method(
+            "exp/1",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(
+                    new RuntimeValue.Method.MethodParameter(
+                        "x",
+                        new RuntimeValue.Primitive(new BigDecimal("0.0")),
+                        false,
+                        false
+                    )
+                ),
+                new RuntimeValue.Primitive(new BigDecimal("0.0"))
+            ),            
+            (Scope methodScope) -> {
+                BigDecimal num = RuntimeValue.requireType(RuntimeValue.requireType(methodScope.lookup("x").get(), RuntimeValue.Variable.class), BigDecimal.class);
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive(BigDecimalMath.exp(num, new MathContext(20)));
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
+
+        // https://www.freepascal.org/docs-html/rtl/system/ln.html
+        scope.define("ln/1", new RuntimeValue.Method(
+            "ln/1",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(
+                    new RuntimeValue.Method.MethodParameter(
+                        "x",
+                        new RuntimeValue.Primitive(new BigDecimal("0.0")),
+                        false,
+                        false
+                    )
+                ),
+                new RuntimeValue.Primitive(new BigDecimal("0.0"))
+            ),            
+            (Scope methodScope) -> {
+                BigDecimal num = RuntimeValue.requireType(RuntimeValue.requireType(methodScope.lookup("x").get(), RuntimeValue.Variable.class), BigDecimal.class);
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive(BigDecimalMath.log(num, new MathContext(20)));
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
+        
+        // https://www.dcs.ed.ac.uk/home/SUNWspro/3.0/pascal/lang_ref/ref_builtin.doc.html
+        scope.define("odd/1", new RuntimeValue.Method(
+            "odd/1",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(
+                    new RuntimeValue.Method.MethodParameter(
+                        "x",
+                        new RuntimeValue.Primitive(new BigInteger("0")),
+                        false,
+                        false
+                    )
+                ),
+                new RuntimeValue.Primitive(Boolean.valueOf(true))
+            ),
+            (Scope methodScope) -> {
+                BigInteger num = RuntimeValue.requireType(RuntimeValue.requireType(methodScope.lookup("x").get(), RuntimeValue.Variable.class), BigInteger.class);
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive(Boolean.valueOf(!num.testBit(0)));
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
+
+        // https://www.freepascal.org/docs-html/rtl/system/pi.html
+        scope.define("pi/0", new RuntimeValue.Method(
+            "pi/0",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(),
+                new RuntimeValue.Primitive(new BigDecimal("0.0"))
+            ),
+            (Scope methodScope) -> {
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive(BigDecimalMath.pi(new MathContext(20)).setScale(20));
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
+
+        
+        // https://www.freepascal.org/docs-html/rtl/system/round.html
+        scope.define("round/1", new RuntimeValue.Method(
+            "round/1",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(
+                    new RuntimeValue.Method.MethodParameter(
+                        "x",
+                        new RuntimeValue.Primitive(new BigDecimal("0.0")),
+                        false,
+                        false
+                    )
+                ),
+                new RuntimeValue.Primitive(new BigInteger("0"))
+            ),
+            (Scope methodScope) -> {
+                BigDecimal num = RuntimeValue.requireType(RuntimeValue.requireType(methodScope.lookup("x").get(), RuntimeValue.Variable.class), BigDecimal.class);
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive(num.setScale(0, RoundingMode.HALF_EVEN).toBigIntegerExact());
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
+
+        
+        // https://www.freepascal.org/docs-html/rtl/system/sin.html
+        scope.define("sin/1", new RuntimeValue.Method(
+            "sin/1",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(
+                    new RuntimeValue.Method.MethodParameter(
+                        "theta",
+                        new RuntimeValue.Primitive(new BigDecimal("0.0")),
+                        false,
+                        false
+                    )
+                ),
+                new RuntimeValue.Primitive(new BigDecimal("0.0"))
+            ),
+            (Scope methodScope) -> {
+                BigDecimal num = RuntimeValue.requireType(RuntimeValue.requireType(methodScope.lookup("theta").get(), RuntimeValue.Variable.class), BigDecimal.class);
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive(BigDecimalMath.sin(num, new MathContext(20)));
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
+
+        
+        // https://www.freepascal.org/docs-html/rtl/system/sqrt.html
+        scope.define("sqrt/1", new RuntimeValue.Method(
+            "sqrt/1",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(
+                    new RuntimeValue.Method.MethodParameter(
+                        "x",
+                        new RuntimeValue.Primitive(new BigDecimal("0.0")),
+                        false,
+                        false
+                    )
+                ),
+                new RuntimeValue.Primitive(new BigDecimal("0.0"))
+            ),
+            (Scope methodScope) -> {
+                BigDecimal num = RuntimeValue.requireType(RuntimeValue.requireType(methodScope.lookup("x").get(), RuntimeValue.Variable.class), BigDecimal.class);
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive(BigDecimalMath.sqrt(num, new MathContext(20)));
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
+        
+        // https://www.freepascal.org/docs-html/rtl/system/trunc.html
+        scope.define("trunc/1", new RuntimeValue.Method(
+            "trunc/1",
+            new RuntimeValue.Method.MethodSignature(
+                List.of(
+                    new RuntimeValue.Method.MethodParameter(
+                        "x",
+                        new RuntimeValue.Primitive(new BigDecimal("0.0")),
+                        false,
+                        false
+                    )
+                ),
+                new RuntimeValue.Primitive(new BigInteger("0"))
+            ),
+            (Scope methodScope) -> {
+                BigDecimal num = RuntimeValue.requireType(RuntimeValue.requireType(methodScope.lookup("x").get(), RuntimeValue.Variable.class), BigDecimal.class);
+                RuntimeValue.Primitive result = new RuntimeValue.Primitive(num.setScale(0, RoundingMode.DOWN).toBigIntegerExact());
+                RuntimeValue.requireType(methodScope.lookup("result").get(), RuntimeValue.Variable.class).setValue(result);
+            })
+        );
 
         return scope;
     }
 
     /**
      * Prints the provided values to the console, one after another, with no ending newline character.
-     * 
+    * 
      * @return a RuntimeValue.Primitive() with value null. 
      */
     private static void write(Scope methodScope, int numParams) {
