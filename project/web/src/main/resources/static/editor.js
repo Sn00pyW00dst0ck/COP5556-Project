2a5cff8c3d65ba95c1646ad1070f544fd0b0dd53
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-04-12 13:42:12 -0400

    Editor.js

---
+++ project/web/src/main/resources/static/editor.js
@@ -0,0 +1,23 @@
+/**
+ * Use a CDN to initialize a Monaco Editor within a div of given ID. 
+ * 
+ * @param {string} containerId the id of the container to initialize the editor into
+ * @returns the monaco editor instance once it has been setup.
+ */
+const initEditor = async (containerId) => {
+    return new Promise((resolve) => {
+        require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs' } });
+
+        require(['vs/editor/editor.main'], function () {
+            const editor = monaco.editor.create(document.getElementById(containerId), {
+                value: 'console.log("Hello from module-like setup!")',
+                language: '',
+                theme: 'vs-dark'
+            });
+            resolve(editor);
+        });
+    });
+};
+  
+// Simulate Export
+window.MonacoEditorModule = { initEditor };
