bd004a7cfac1ab49de7968944bb6300095e49a03
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-04-26 14:26:59 -0400

    Update LLVMValue implementations

--- project/core/src/main/java/plp/group/Compiler/LLVMValue.java
+++ project/core/src/main/java/plp/group/Compiler/LLVMValue.java
@@ -14,6 +14,36 @@ public interface LLVMValue {
     // TODO: representations for register, function, struct, etc...
 
     /**
+     * Represents a register (something like %tmp, %1, etc).
+     */
+    public record Register(
+        java.lang.String name,
+        java.lang.String type
+    ) implements LLVMValue {
+        public java.lang.String getType() { return type; }
+        public java.lang.String getRef() { return name; }
+    }
+
+    public record Global(
+        java.lang.String name,
+        java.lang.String type
+    ) implements LLVMValue {
+        public java.lang.String getType() { return type; }
+        public java.lang.String getRef() { return "@" + name; }
+    }
+
+    /**
+     * Represents a constant value for immediate use (except for strings which are separate).
+     */
+    public record Immediate(
+        java.lang.String value,
+        java.lang.String type
+    ) implements LLVMValue {
+        public java.lang.String getType() { return type; }
+        public java.lang.String getRef() { return value; }
+    }
+
+    /**
      * Represents a String LLVMValue...
      */
     public record String(
@@ -22,5 +52,41 @@ public interface LLVMValue {
     ) implements LLVMValue {
         public java.lang.String getType() { return "[" + length + " x i8]"; }
         public java.lang.String getRef() { return "@" + name; }
+    }
+
+    /**
+     * Represents a pointer to another LLVMValue.
+     * 
+     * TODO: getType and getRef here don't make as much sense, gotta figure something out
+     */
+    public record Pointer(
+        LLVMValue pointee
+    ) implements LLVMValue {
+        public java.lang.String getType() { return pointee.getType() + "*"; }
+        public java.lang.String getRef() { return pointee.getRef(); }
+    }
+
+    /**
+     * Represents a function in LLVM
+     * 
+     * TODO: figure out getType becuase idk what it should be.
+     */
+    public record Function(
+        java.lang.String name,
+        java.lang.String type
+    ) implements LLVMValue {
+        public java.lang.String getType() { return type; }
+        public java.lang.String getRef() { return "@" + name; }
+    }
+
+    /**
+     * Represents a struct in LLVM
+     * 
+     * TODO: figure out what a struct will need to look like in the symbol table...
+     */
+    public record Struct(
+    ) implements LLVMValue {
+        public java.lang.String getType() { return ""; }
+        public java.lang.String getRef() { return ""; }
     }
 }
