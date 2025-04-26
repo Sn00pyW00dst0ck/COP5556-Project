c9f7922c08796e6d27f8616c156320e035507f90
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-04-26 14:06:43 -0400

    Compiler Setup

---
+++ project/core/src/main/java/plp/group/Compiler/CompilerContext.java
@@ -0,0 +1,24 @@
+package plp.group.Compiler;
+
+import java.util.Optional;
+
+/**
+ * An object that should be passed around the compiler passes to serve as a collection of all the compiler state...
+ */
+public class CompilerContext {
+    /**
+     * The internal represetntation string builder where IR is written.
+     */
+    public final StringBuilder ir = new StringBuilder();
+
+    /**
+     * The symbols in use...
+     */
+    public final SymbolTable symbolTable = new SymbolTable(Optional.empty());
+
+    private int tempCounter = 0;
+    private int labelCounter = 0;
+
+    public String getNextTmp() { return "%tmp" + (tempCounter++); }
+    public String getNextLabel() { return "" + (labelCounter++); }
+}
---
+++ project/core/src/main/java/plp/group/Compiler/SymbolTable.java
@@ -0,0 +1,61 @@
+package plp.group.Compiler;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+/**
+ * A symbol table, analagous to a scope, but usable in more contexts...
+ */
+public class SymbolTable {
+    private final Optional<SymbolTable> parent;
+    private final HashMap<String, LLVMValue> symbols = new HashMap<String, LLVMValue>();
+
+    public SymbolTable(Optional<SymbolTable> parent) {
+        this.parent = parent;
+    }
+
+    /**
+     * Define a new symbol to the symbol table.
+     * 
+     * @param name the name to define
+     * @param value the value to define the name as
+     */
+    public void define(String name, LLVMValue value) {
+        symbols.put(name, value);
+    }
+
+    /**
+     * Look up a LLVMValue 'symbol' by name within the symbol table.
+     * 
+     * @param name the name to look up.
+     * @param current set true if looking in only the current scope!
+     * @return the LLVMValue within an optional.
+     */
+    public Optional<LLVMValue> lookup(String name, boolean current) {
+        if (symbols.containsKey(name)) {
+            return Optional.of(symbols.get(name));
+        }
+        if (parent.isPresent() && !current) {
+            return parent.get().lookup(name, false);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Get a new scope (an empty symbol table whose parent is set to the instance you called pushScope on).
+     * 
+     * @return the new scope (symbol table)
+     */
+    public SymbolTable pushScope() {
+        return new SymbolTable(Optional.of(this));
+    }
+
+    /**
+     * Return to the parent scope by getting its reference.
+     * 
+     * @return the parent scope (symbol table)
+     */
+    public Optional<SymbolTable> popScope() {
+        return parent;
+    }
+}
