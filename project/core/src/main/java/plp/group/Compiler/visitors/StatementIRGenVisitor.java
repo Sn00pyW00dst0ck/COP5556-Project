a8ffeb14183f3ebae130a8e1676f835d53d674b9
Sn00pyW00dst0ck <gabealdous@gmail.com>
2025-04-28 15:07:05 -0400

    Statement IR Generation Updated For Types

--- project/core/src/main/java/plp/group/Compiler/visitors/StatementIRGenVisitor.java
+++ project/core/src/main/java/plp/group/Compiler/visitors/StatementIRGenVisitor.java
@@ -34,34 +34,45 @@ public class StatementIRGenVisitor extends ASTBaseVisitor<Object> {
     //#region Expressions - returns the 'temp' they create as a string...
 
     @Override
-    public String visitExpressionBinary(AST.Expression.Binary expr) {
+    public LLVMValue visitExpressionBinary(AST.Expression.Binary expr) {
         // Visit the LHS and RHS, generating the IR to compute them and getting their 'tmp' result.
-        String lhsTemp = (String) this.visit(expr.lhs());
-        String rhsTemp = (String) this.visit(expr.rhs());
+        LLVMValue lhsTemp = (LLVMValue) this.visit(expr.lhs());
+        LLVMValue rhsTemp = (LLVMValue) this.visit(expr.rhs());
+
+        // TODO: figure out type calculation...
+        String type = switch (lhsTemp.getType()) {
+            case "double" -> "double";
+            case "i32" -> {
+                yield rhsTemp.getType().equals("double") ? "double" : "i32";
+            }
+            case "i8" -> "i8";
+            case "i1" -> "i1";
+            default -> "UNKNOWN"; // Placeholder for unknown types... they need to be handled too
+        };
 
         // Generate an instruction based on LHS, RHS, and operand, then put result in a tmp...
-        String tmp = context.getNextTmp();
+        LLVMValue tmp = new LLVMValue.Register(context.getNextTmp(), type);
+        context.symbolTable.define(tmp.getRef(), tmp);
 
-        context.ir.append(tmp + " = ");
+        context.ir.append(tmp.getRef() + " = ");
         context.ir.append(
             switch (expr.operator()) {
-                // I'm assuming integers for now, TODO: figure out types...
-                case "+" -> "add i32 " + lhsTemp + ", " + rhsTemp;
-                case "-" -> "sub i32 " + lhsTemp + ", " + rhsTemp;
-                case "*" -> "mul i32 " + lhsTemp + ", " + rhsTemp;
-                case "/" -> "div i32 " + lhsTemp + ", " + rhsTemp;
-                case "DIV" -> "sdiv i32 " + lhsTemp + ", " + rhsTemp;
-                case "MOD" -> "srem i32 " + lhsTemp + ", " + rhsTemp;
+                // I'm assuming integers for now, TODO: figure out types i vs f instructions...
+                case "+" -> (tmp.getType() == "double" ? "f" : "") + "add " + tmp.getType() + " " + lhsTemp.getRef() + ", " + rhsTemp.getRef();
+                case "-" -> (tmp.getType() == "double" ? "f" : "") + "sub " + tmp.getType() + " " + lhsTemp.getRef() + ", " + rhsTemp.getRef();
+                case "*" -> (tmp.getType() == "double" ? "f" : "") + "mul " + tmp.getType() + " " + lhsTemp.getRef() + ", " + rhsTemp.getRef();
+                case "DIV", "/" -> (tmp.getType() == "double" ? "f" : "s") + "div " + tmp.getType() + " " + lhsTemp.getRef() + ", " + rhsTemp.getRef();
+                case "MOD" -> (tmp.getType() == "double" ? "f" : "s") + "rem " + tmp.getType() + " " + lhsTemp.getRef() + ", " + rhsTemp.getRef();
 
-                case "=" -> "icmp eq " + lhsTemp + ", " + rhsTemp;
-                case "<>" -> "icmp ne " + lhsTemp + ", " + rhsTemp;
-                case "<" -> "icmp slt " + lhsTemp + ", " + rhsTemp;
-                case "<=" -> "icmp sle " + lhsTemp + ", " + rhsTemp;
-                case ">" -> "icmp sgt " + lhsTemp + ", " + rhsTemp;
-                case ">=" -> "icmp sge " + lhsTemp + ", " + rhsTemp;
+                case "=" -> "icmp eq " + tmp.getRef() + ", " + rhsTemp.getRef();
+                case "<>" -> "icmp ne " + tmp.getRef() + ", " + rhsTemp.getRef();
+                case "<" -> "icmp slt " + tmp.getRef() + ", " + rhsTemp.getRef();
+                case "<=" -> "icmp sle " + tmp.getRef() + ", " + rhsTemp.getRef();
+                case ">" -> "icmp sgt " + tmp.getRef() + ", " + rhsTemp.getRef();
+                case ">=" -> "icmp sge " + tmp.getRef() + ", " + rhsTemp.getRef();
 
-                case "AND" -> "and i1 " + lhsTemp + ", " + rhsTemp;
-                case "OR" -> "or i1 " + lhsTemp + ", " + rhsTemp ;
+                case "AND" -> "and i1 " + tmp.getRef() + ", " + rhsTemp.getRef();
+                case "OR" -> "or i1 " + tmp.getRef() + ", " + rhsTemp.getRef();
 
                 // TODO: OTHER CASES IF APPLICABLE
                 default -> throw new RuntimeException("Unexpected operator: " + expr.operator());
@@ -74,21 +85,22 @@ public class StatementIRGenVisitor extends ASTBaseVisitor<Object> {
     }
 
     @Override
-    public String visitExpressionUnary(AST.Expression.Unary expr) {
+    public LLVMValue visitExpressionUnary(AST.Expression.Unary expr) {
         // Visit the value first, then generate an instruction based on value and operand.
-        String exprTemp = (String) this.visit(expr.expression());
+        LLVMValue exprTemp = (LLVMValue) this.visit(expr.expression());
 
         // Generate an instruction based on LHS, RHS, and operand, then put result in a tmp...
-        String tmp = context.getNextTmp();
+        LLVMValue tmp = new LLVMValue.Register(context.getNextTmp(), exprTemp.getType());
+        context.symbolTable.define(tmp.getRef(), tmp);
 
-        context.ir.append(tmp + " = ");
+        context.ir.append(tmp.getRef() + " = ");
         context.ir.append(
             switch (expr.operator()) {
                 // I'm assuming integers for now, TODO: figure out types...
-                case "+" -> "";
-                case "-" -> "";
+                case "+" -> (tmp.getType() == "double" ? "f" : "") + "add " + tmp.getType() + " 0, " + exprTemp.getRef();
+                case "-" -> (tmp.getType() == "double" ? "f" : "") + "sub " + tmp.getType() + " 0, " + exprTemp.getRef();
 
-                case "NOT" -> "xor i1 " + exprTemp + ", 1"; // XOR with a 1 always flips the bit
+                case "NOT" -> "xor i1 " + exprTemp.getRef() + ", 1"; // XOR with a 1 always flips the bit
                 // TODO: OTHER CASES IF APPLICABLE
                 default -> throw new RuntimeException("Unexpected operator: " + expr.operator());
             }
@@ -99,17 +111,17 @@ public class StatementIRGenVisitor extends ASTBaseVisitor<Object> {
     }
 
     @Override
-    public String visitExpressionGroup(AST.Expression.Group expr) {
-        return (String) this.visit(expr.expression());
+    public LLVMValue visitExpressionGroup(AST.Expression.Group expr) {
+        return (LLVMValue) this.visit(expr.expression());
     }
 
     @Override
-    public String visitExpressionVariable(AST.Expression.Variable expr) {
+    public LLVMValue visitExpressionVariable(AST.Expression.Variable expr) {
         // COMPLICATED AS HELL!
         // For something with no postFix, do a simple lookup in scope (aka a load I think)...
         // For something with a postFix, need to generate more complicated IR...
         System.out.println(expr);
-        return "";
+        return null;
     }
 
     //#endregion Expressions
@@ -117,34 +129,34 @@ public class StatementIRGenVisitor extends ASTBaseVisitor<Object> {
     //#region Literals - returns their literal value as a string...
 
     @Override
-    public String visitExpressionLiteralInteger(AST.Expression.Literal.Integer lit) {
-        return lit.value().toString(10);
+    public LLVMValue.Immediate visitExpressionLiteralInteger(AST.Expression.Literal.Integer lit) {
+        return new LLVMValue.Immediate(lit.value().toString(10), "i32");
     }
 
     @Override
-    public String visitExpressionLiteralReal(AST.Expression.Literal.Real lit) {
-        return lit.value().toString();
+    public LLVMValue.Immediate visitExpressionLiteralReal(AST.Expression.Literal.Real lit) {
+        return new LLVMValue.Immediate(lit.value().toString(), "double");
     }
 
     @Override
-    public String visitExpressionLiteralString(AST.Expression.Literal.String lit) {
+    public LLVMValue.Immediate visitExpressionLiteralString(AST.Expression.Literal.String lit) {
         LLVMValue value = context.symbolTable.lookup("'" + lit.value() + "'", false).get();
-        return value.getRef();
+        return new LLVMValue.Immediate(value.getRef(), value.getType());
     }
 
     @Override
-    public String visitExpressionLiteralCharacter(AST.Expression.Literal.Character lit) {
-        return "" + (int) lit.value().charValue();
+    public LLVMValue.Immediate visitExpressionLiteralCharacter(AST.Expression.Literal.Character lit) {
+        return new LLVMValue.Immediate("" + (int) lit.value().charValue(), "i8");
     }
 
     @Override
-    public String visitExpressionLiteralBoolean(AST.Expression.Literal.Boolean lit) {
-        return "" + (lit.value() ? 1 : 0);
+    public LLVMValue.Immediate visitExpressionLiteralBoolean(AST.Expression.Literal.Boolean lit) {
+        return new LLVMValue.Immediate("" + (lit.value() ? 1 : 0), "i1");
     }
 
     @Override
-    public String visitExpressionLiteralNil(AST.Expression.Literal.Nil lit) {
-        return "null";
+    public LLVMValue.Immediate visitExpressionLiteralNil(AST.Expression.Literal.Nil lit) {
+        return new LLVMValue.Immediate("null", null);
     }
 
     //#endregion Literals
